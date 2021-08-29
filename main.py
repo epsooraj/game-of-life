6bf04567a704d282a62aeba16ad11ca566c73a76
Sooraj Ep <epsooraj4@gmail.com>
2021-08-29 22:58:58 +0530

    Rearranging and patches

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import sys
-import pygame
-from pygame.locals import KEYDOWN, K_q
-import numpy as np
 
-from gol import generate_next_state
+import pygame
+from pygame.constants import MOUSEWHEEL, K_n
+from pygame.locals import KEYDOWN, K_q
+
+from gol import generate_next_state, initialize_life_state
+
+# Initialize Life State with pattern
+cellMAP = initialize_life_state()
 
 # CONSTANTS:
 SCREENSIZE = WIDTH, HEIGHT = 1000, 1000
-BLACK = (0, 0, 0)
-GREY = (160, 160, 160)
-
-# OUR GRID MAP:
-# cellMAP = np.random.randint(2, size=(50, 50))
-cellMAP = np.eye(50, dtype=int)
+ALIVE_COLOR = (255, 255, 255)
+DEAD_COLOR = (0, 0, 0)
 
 _VARS = {'surf': False, 'gridWH': 1000,
-         'gridOrigin': (0, 0), 'gridCells': cellMAP.shape[0], 'lineWidth': 2}
+         'gridOrigin': (0, 0), 'gridCells': cellMAP.shape[0], 'lineWidth': 0}
 
-FPS = 1
+FPS = 60
 
 
 def main():
@@ -26,30 +26,33 @@ def main():
     global _VARS
 
     pygame.init()
-    pygame.display.set_caption('Math')
+    pygame.display.set_caption('Game of Life')
     clock = pygame.time.Clock()
     _VARS['surf'] = pygame.display.set_mode(SCREENSIZE)
     while True:
-        cellMAP = generate_next_state(cellMAP)
-        checkEvents()
-        _VARS['surf'].fill(GREY)
-        _VARS['gridCells'] = cellMAP.shape[0]
+        frame_step = checkEvents()
+        _VARS['surf'].fill(DEAD_COLOR)
         drawSquareGrid(
             _VARS['gridOrigin'], _VARS['gridWH'], _VARS['gridCells'])
         placeCells()
         pygame.display.update()
+
+        # if frame_step:
+        cellMAP = generate_next_state(cellMAP)
+
         clock.tick(FPS)
 
 
-# NEW METHOD FOR ADDING CELLS :
 def placeCells():
-    # GET CELL DIMENSIONS...
+    '''
+    METHOD FOR ADDING CELLS
+    '''
     cellBorder = 1
+    # GET CELL DIMENSIONS
     celldimX = celldimY = (_VARS['gridWH']/_VARS['gridCells']) - (cellBorder*2)
-    # DOUBLE LOOP
+
     for row in range(cellMAP.shape[0]):
         for column in range(cellMAP.shape[1]):
-            # Is the grid cell tiled ?
             if(cellMAP[column][row] == 1):
                 drawSquareCell(
                     _VARS['gridOrigin'][0] + (celldimY*row)
@@ -59,12 +62,13 @@ def placeCells():
                     _VARS['lineWidth']/2,
                     celldimX, celldimY)
 
-# Draw filled rectangle at coordinates
-
 
 def drawSquareCell(x, y, dimX, dimY):
+    '''
+    Draw filled rectangle at coordinates
+    '''
     pygame.draw.rect(
-        _VARS['surf'], BLACK,
+        _VARS['surf'], ALIVE_COLOR,
         (x, y, dimX, dimY)
     )
 
@@ -77,23 +81,23 @@ def drawSquareGrid(origin, gridWH, cells):
     # DRAW Grid Border:
     # TOP lEFT TO RIGHT
     pygame.draw.line(
-        _VARS['surf'], BLACK,
+        _VARS['surf'], ALIVE_COLOR,
         (cont_x, cont_y),
         (CONTAINER_WIDTH_HEIGHT + cont_x, cont_y), _VARS['lineWidth'])
     # # BOTTOM lEFT TO RIGHT
     pygame.draw.line(
-        _VARS['surf'], BLACK,
+        _VARS['surf'], ALIVE_COLOR,
         (cont_x, CONTAINER_WIDTH_HEIGHT + cont_y),
         (CONTAINER_WIDTH_HEIGHT + cont_x,
             CONTAINER_WIDTH_HEIGHT + cont_y), _VARS['lineWidth'])
     # # LEFT TOP TO BOTTOM
     pygame.draw.line(
-        _VARS['surf'], BLACK,
+        _VARS['surf'], ALIVE_COLOR,
         (cont_x, cont_y),
         (cont_x, cont_y + CONTAINER_WIDTH_HEIGHT), _VARS['lineWidth'])
     # # RIGHT TOP TO BOTTOM
     pygame.draw.line(
-        _VARS['surf'], BLACK,
+        _VARS['surf'], ALIVE_COLOR,
         (CONTAINER_WIDTH_HEIGHT + cont_x, cont_y),
         (CONTAINER_WIDTH_HEIGHT + cont_x,
             CONTAINER_WIDTH_HEIGHT + cont_y), _VARS['lineWidth'])
@@ -104,23 +108,33 @@ def drawSquareGrid(origin, gridWH, cells):
     # VERTICAL DIVISIONS: (0,1,2) for grid(3) for example
     for x in range(cells):
         pygame.draw.line(
-            _VARS['surf'], BLACK,
+            _VARS['surf'], ALIVE_COLOR,
             (cont_x + (cellSize * x), cont_y),
-            (cont_x + (cellSize * x), CONTAINER_WIDTH_HEIGHT + cont_y), 2)
+            (cont_x + (cellSize * x), CONTAINER_WIDTH_HEIGHT + cont_y), 0)
     # # HORIZONTAl DIVISIONS
         pygame.draw.line(
-            _VARS['surf'], BLACK,
+            _VARS['surf'], ALIVE_COLOR,
             (cont_x, cont_y + (cellSize*x)),
-            (cont_x + CONTAINER_WIDTH_HEIGHT, cont_y + (cellSize*x)), 2)
+            (cont_x + CONTAINER_WIDTH_HEIGHT, cont_y + (cellSize*x)), 0)
 
 
 def checkEvents():
+    '''
+    Keyboard, Mouse events
+    '''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == KEYDOWN and event.key == K_q:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYDOWN and event.key == K_n:
+            return True
+        elif event.type == MOUSEWHEEL:
+            print(event)
+            print(event.x, event.y)
+            print(event.flipped)
+            print(event.which)
 
 
 if __name__ == '__main__':
